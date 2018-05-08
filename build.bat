30482dd38f862f1583813a9770dc060611e4911a
lotomer <lotomer@163.com>
2018-05-08 12:36:28 +0800

    一键编译打包多个版本

---
+++ build.bat
@@ -0,0 +1,23 @@
+@echo off
+rem 作者：Lotomer
+rem 时间：2018-05-08
+rem 最后更新时间：2018-05-08
+set CURR_PATH=%~dp0
+cd %CURR_PATH%
+call :buildGO datastore linux amd64 "" ".\application\datastore"
+call :buildGO datastore linux 386 "" ".\application\datastore"
+call :buildGO datastore windows amd64 ".exe" ".\application\datastore"
+
+goto :exit
+
+:buildGO
+set APPNAME=%~1
+set GOOS=%~2
+set GOARCH=%~3
+set GOEXE=%~4
+set SRCPATH=%~5
+echo go build -o build\%APPNAME%-%GOOS%-%GOARCH%%GOEXE% %SRCPATH%
+go build -o build\%APPNAME%-%GOOS%-%GOARCH%%GOEXE% %SRCPATH%
+goto :eof
+:exit
+pause
