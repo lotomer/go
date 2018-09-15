534b5f9bfc2c57f48a02aef6de4e20c60818a6e5
lotomer <lotomer@163.com>
2018-09-15 19:57:20 +0800

    增加程序自动启动到后台的功能

--- application/common/http.main.go
+++ application/common/http.main.go
@@ -8,6 +8,9 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"os/exec"
+	"path/filepath"
+	"strconv"
 
 	"github.com/lotomer/go/common"
 	"github.com/lotomer/go/config"
@@ -27,6 +30,18 @@ var accessControlAllowOrigin = flag.String("Access-Control-Allow-Origin", "", "T
 
 //Main 提供给通用http服务入口
 func Main(name string, fun func(*sql.DB)) {
+	pid := os.Getpid()
+	if os.Getppid() != 1 { //判断当其是否是子进程，当父进程return之后，子进程会被 系统1 号进程接管
+		filePath, _ := filepath.Abs(os.Args[0]) //将命令行参数中执行文件路径转换成可用路径
+		cmd := exec.Command(filePath, os.Args[1:]...)
+		//将其他命令传入生成出的进程
+		cmd.Stdin = os.Stdin //给新进程设置文件描述符，可以重定向到文件中
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		cmd.Start() //开始执行新进程，不等待新进程退出
+		return
+	}
+
 	// 解析命令行参数
 	flag.Parse()
 	if *help {
@@ -38,7 +53,7 @@ func Main(name string, fun func(*sql.DB)) {
 		var noneWriter common.NoneWriter
 		log.SetOutput(&noneWriter)
 	} else {
-		log.SetPrefix("[" + name + "] ")
+		log.SetPrefix("[" + name + "-" + strconv.Itoa(pid) + "] ")
 	}
 	// 设置全局变量
 	common.GlobalConfig.AccessControlAllowOrigin = *accessControlAllowOrigin
